121e093d076c39879d559179c08b4b82edb9236c
Léa Strobino <leastrobino.noreply@gmail.com>
2017-09-08 14:48:15 +0200

    Added macOS support (Wine)

--- svg2tpx.m
+++ svg2tpx.m
@@ -8,14 +8,26 @@
 function svg2tpx(infile,outfile)
 persistent TpX
 if isempty(TpX)
-  TpX = 'C:\Program Files\TpX\TpX.exe';
-  if ~exist(TpX,'file')
-    TpX = 'C:\Program Files (x86)\TpX\TpX.exe';
-    if ~exist(TpX,'file')
-      url = 'http://tpx.sourceforge.net';
-      error('svg2tpx:TpXNotFound',...
-        'TpX not found. Have you installed it? See <a href="%s">%s</a>.',url,url);
+  s = 0;
+  if ismac
+    wine = '/Applications/Wine Stable.app/Contents/Resources/wine/bin/wine';
+    TpX = '/Applications/TpX.app/Contents/Resources/TpX.exe';
+    if exist(wine,'file') && exist(TpX,'file')
+      TpX = [wine '" "' TpX];
+      s = 1;
     end
+  else
+    TpX = 'C:\Program Files\TpX\TpX.exe';
+    s = exist(TpX,'file');
+    if ~s
+      TpX = 'C:\Program Files (x86)\TpX\TpX.exe';
+      s = exist(TpX,'file');
+    end
+  end
+  if ~s
+    url = 'http://tpx.sourceforge.net';
+    error('svg2tpx:TpXNotFound',...
+      'TpX not found. Have you installed it? See <a href="%s">%s</a>.',url,url);
   end
 end
 [~,~] = system(['"' TpX '" "' infile '" -o "' outfile '" -m tikz']);
